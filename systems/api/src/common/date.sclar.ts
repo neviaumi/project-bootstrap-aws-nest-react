f7d2d63a11e9636b5c187aaa94513ed45b299b85
neviaumi <david.ng.dev@gmail.com>
2023-11-02 18:53:37 +0000

    ISSUE-1172: update luxon package and introduce error handling
    this close #1172

--- systems/api/src/common/date.sclar.ts
+++ systems/api/src/common/date.sclar.ts
@@ -2,25 +2,50 @@ import { CustomScalar, Scalar } from '@nestjs/graphql';
 import { Kind, ValueNode } from 'graphql';
 import { DateTime } from 'luxon';
 
+import { ApolloException } from '../error-hanlding/apollo.exception';
+import { ErrorCode } from '../error-hanlding/error-code.constant';
+
 @Scalar('Date', () => Date)
 export class DateScalar implements CustomScalar<string, Date> {
   description = 'Date custom scalar type';
 
   parseValue(value: unknown): Date {
-    return new Date(value as number); // value from the client
+    const luxonDate = DateTime.fromJSDate(new Date(value as number));
+    if (!luxonDate.isValid) {
+      throw new ApolloException({
+        code: ErrorCode.ValidationError,
+        errors: [
+          {
+            detail: 'Invalid Date on Input',
+            title: 'Validation Error',
+          },
+        ],
+      });
+    }
+    return luxonDate.toJSDate(); // value from the client
   }
 
   serialize(value: unknown): string {
-    return DateTime.fromJSDate(value as Date).toISODate(); // value sent to the client
+    const isoDateString = DateTime.fromJSDate(value as Date).toISODate(); // value sent to the client
+    if (!isoDateString) {
+      throw new ApolloException({
+        code: ErrorCode.ValidationError,
+        errors: [
+          {
+            detail: 'Invalid Date on Response',
+            title: 'Validation Error',
+          },
+        ],
+      });
+    }
+    return isoDateString;
   }
 
   parseLiteral(ast: ValueNode): Date {
     if (ast.kind === Kind.STRING) {
       return DateTime.fromISO(ast.value as string).toJSDate();
     }
-    // @ts-except-error default return null from NestJS Example
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
+    // @ts-expect-error default return null from NestJS Example
     return null;
   }
 }
